064c0b4261d42cb6cdb74912f16691cb2161445c
Phakkharaphong <phakkharaphong.c@kkumail.com>
2025-08-25 22:42:45 +0700

    ๊Update Method subport Array

--- dr_roomlist_get.py
+++ dr_roomlist_get.py
@@ -85,11 +85,71 @@ def Get_APIEndPoint_Province(url_point,province_name):
             print(f"Total records: {len(records)}")
 
 
+#GET_API Ony
+def Get_APIEndPoint_Only(url_point):
+    url = f"{url_point}"
+    print(f"Fetching: {url}")
 
-path_file_excel = input("Enter your path file excel for read: ")
-sheet_name = input("Enter your sheet_name in excel : ")
+    output_dir = "C:\\Users\\tyuoi\\OneDrive\\เดสก์ท็อป\\ETLDATA\\Project_ETL_Data_From_REST_API\\APIระบบส้วม"
+    output_file = "output_Result.csv"
+    output_path = os.path.join(output_dir, output_file)
+
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Failed to fetch data. Status code: {response.status_code}")
+        return
+
+    data = response.json()
+    print(f"Response type: {type(data)}")
+
+    # --- Case 1: ถ้า data เป็น list of dict ---
+    if isinstance(data, list) and all(isinstance(item, dict) for item in data):
+        records = data
+
+    # --- Case 2: ถ้า data เป็น dict และมี "data" ---
+    elif isinstance(data, dict) and "data" in data and "items" in data["data"]:
+        #เลือกรายการ Array
+        records = data["data"]["items"]
+    else:
+        print("Unexpected data format")
+        return
+
+    # --- Flatten Array ---
+    flattened_records = []
+    all_assessment_headers = set()
+
+    for item in records:
+        base = {k: v for k, v in item.items() if k != "assessments"}
+        if "assessments" in item and isinstance(item["assessments"], list):
+            for assess in item["assessments"]:
+                # ใช้ wcStandardCriteriaName เป็น header
+                base[assess["wcStandardCriteriaName"]] = assess["isPass"]
+                all_assessment_headers.add(assess["wcStandardCriteriaName"])
+        flattened_records.append(base)
+
+    # สร้าง headers = key ปกติ + key จาก assessments
+    normal_headers = [k for k in flattened_records[0].keys() if k not in all_assessment_headers]
+    headers = normal_headers + list(all_assessment_headers)
+
+    # --- Export To CSV ---
+    os.makedirs(output_dir, exist_ok=True)
+    with open(output_path, "w", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=headers)
+        writer.writeheader()
+        writer.writerows(flattened_records)
+
+    print(f"✅ Data successfully saved to {output_path}")
+    print(f"Total records: {len(flattened_records)}")
+
+
+#========================== Input ===================================
+#path_file_excel = input("Enter your path file excel for read: ")
+#sheet_name = input("Enter your sheet_name in excel : ")
 url_API = input("Enter your path API End point: ")
 #sheet_name = input("Enter your sheet_name in excel : ")
-Read_excel(path_file_excel,sheet_name,url_API)
+
+#========================= Call Method ================
+#Read_excel(path_file_excel,sheet_name,url_API)
+Get_APIEndPoint_Only(url_API)
 
 #Get_APIEndPoint_Province(url_API,"ขอนแก่น")
