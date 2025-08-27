f4426adc69fd6e33a1e7d20c912369c5dcd54b68
Phakkharaphong <phakkharaphong.c@kkumail.com>
2025-08-27 09:33:57 +0700

    Update Code Version 2

--- dr_roomlist_get.py
+++ dr_roomlist_get.py
@@ -14,6 +14,7 @@ import requests
 import csv
 import os
 import pandas as pd
+import json as json
 
 
 
@@ -30,7 +31,7 @@ def Read_excel(path_file_excel, sheet_namee,url_API):
         print(f"แถว {idx} {name}")
         Get_APIEndPoint_Province(url_API,name)
 
-def Get_APIEndPoint_Province(url_point,province_name):
+def Get_API_Food_Province(url_point,province_name):
     # API endpoint
     #url = f"https://foodhandler.anamai.moph.go.th/webapp/webservice/qa?province=ขอนแก่น"
     url = f"{url_point}{province_name}"
@@ -86,7 +87,7 @@ def Get_APIEndPoint_Province(url_point,province_name):
 
 
 #GET_API Ony
-def Get_APIEndPoint_Only(url_point):
+def Get_API_Toilet_Only(url_point):
     url = f"{url_point}"
     print(f"Fetching: {url}")
 
@@ -142,14 +143,74 @@ def Get_APIEndPoint_Only(url_point):
     print(f"Total records: {len(flattened_records)}")
 
 
+
+def ETL_API_System_Food_Only(url_point, month, year):
+    url = f"{url_point}?month={month}&year={year}"
+    print(f"{url}")
+    output_dir = "C:\\Users\\chuwo\\OneDrive\\เอกสาร\\Desktop\\all_project\\Project_ETL_Data_From_REST_API\\APIผู้ผ่านการอบรม"
+    output_file = f"output_Result_ผู้ผ่านการอบรม_{month}{year}.csv"
+    output_path = os.path.join(output_dir, output_file)
+     #Make the API request
+    response = requests.get(url)
+    print(type(response))
+
+    # Validate the response
+    if response.status_code != 200:
+        print(f"Failed to fetch data. Status code: {response.status_code}")
+    else:
+        #Convert Response To text for skip tag php
+        raw_text = response.text
+        json_text = raw_text[raw_text.find("{"):]
+        data = json.loads(json_text)
+        print(type(data))
+    
+        # Check if data is a list of dictionaries
+        if isinstance(data, list) and all(isinstance(item, dict) for item in data):
+            # Ensure output directory exists
+            os.makedirs(output_dir, exist_ok=True)
+
+            # Get headers from the first item
+            headers = data[2].keys()
+
+            # Write to CSV
+            with open(output_path, "w", newline="", encoding="utf-8") as f:
+                writer = csv.DictWriter(f, fieldnames=headers)
+                writer.writeheader()
+                writer.writerows(data)
+
+            print(f"Data successfully saved to {output_path}")
+            print(f"Total records: {len(data)}")
+        else:
+            print("Unexpected data format. Expected a list of dictionaries.")
+            if isinstance(data, dict) and "data" in data:
+                records = data["data"]  # ดึง list ออกมา
+                #records ตอนนี้เป็น list of dict
+                headers = records[1].keys()
+                print(f"Data Is: {headers}")
+
+                # Write to CSV
+                with open(output_path, "w", newline="", encoding="utf-8-sig") as f:
+                    writer = csv.DictWriter(f, fieldnames=headers, delimiter=';')
+                    writer.writeheader()
+                    #records เป็นการชี้ว่าให้เริ่มการเขียนลงไฟล์ CSV เริ่มต้นจากลำดับที่ 1
+                    writer.writerows(records[1:])
+
+                print(f"Data successfully saved to {output_path}")
+                print(f"Total records: {len(records)}")
+
+
+
+
 #========================== Input ===================================
 #path_file_excel = input("Enter your path file excel for read: ")
 #sheet_name = input("Enter your sheet_name in excel : ")
 url_API = input("Enter your path API End point: ")
+month_in = input("Enter Month: ")
+year_in = input("Enter year: ")
 #sheet_name = input("Enter your sheet_name in excel : ")
 
 #========================= Call Method ================
+ETL_API_System_Food_Only(url_API,month_in,year_in)
 #Read_excel(path_file_excel,sheet_name,url_API)
-Get_APIEndPoint_Only(url_API)
-
+#Get_APIEndPoint_Only(url_API)
 #Get_APIEndPoint_Province(url_API,"ขอนแก่น")
